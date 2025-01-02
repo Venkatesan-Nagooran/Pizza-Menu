67acddd027ac43934e6b6723e3ccf3f54be05a3a
Venkatesan <venkatesannagooran@gmail.com>
2025-01-02 19:45:13 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Footer from "./Footer";
+import Header from "./Header";
+import Menu from "./Menu";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
